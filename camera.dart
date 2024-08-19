403537acd4d1872236f6dcfc97c5e0060044d148
Nandanacb <nandhanacb@gmail.com>
2024-08-19 22:27:18 +0530

    oopsassignment 19/7/24

--- camera.dart
+++ camera.dart
@@ -15,45 +15,38 @@ class Camera{
   set color(String color)=>this._color=color;
   set price(int price)=>this._price=price;
 
+void display()
+{
+  print("Id:$id");
+  print("Brand:$id");
+  print("Color:$color");
+  print("Price:$price");
+}
 
 }
 
 void main()
 {
-  Camera cm1=Camera();
-  cm1.id=11;
-  cm1.brand="Sony";
+Camera cm1=Camera();
+  cm1.id=12;
+  cm1.brand="Canon";
   cm1.color="Black";
-  cm1.price=590000;
- 
-  print(cm1.id);
-  print(cm1.brand);
-  print(cm1.color);
-  print(cm1.price);
+  cm1.price=100000;
+  cm1.display();
   
-
-  Camera cm2=Camera();
+ Camera cm2=Camera();
   cm2.id=12;
   cm2.brand="Canon";
   cm2.color="Black";
   cm2.price=100000;
- 
-  print(cm2.id);
-  print(cm2.brand);
-  print(cm2.color);
-  print(cm2.price);
+  cm2.display();
   
-
-  Camera cm3=Camera();
+ Camera cm3=Camera();
   cm3.id=13;
   cm3.brand="Hd";
   cm3.color="Black";
   cm3.price=450000;
- 
-  print(cm3.id);
-  print(cm3.brand);
-  print(cm3.color);
-  print(cm3.price);
+  cm3.display();
   
 
 }
